2420ca0c32878883fab6adae6066e8047e326e1b
Sven Mühlberg <sven@muehlberg.net>
2023-02-27 22:25:59 +0100

    Added WiFIManager Config for WLAN and VNC

--- AZ-Touch_VNC.ino
+++ AZ-Touch_VNC.ino
@@ -9,27 +9,30 @@
  * FT6X36: https://github.com/strange-v/FT6X36.git
  * GT911: https://github.com/TAMCTec/gt911-arduino.git
  * XPT2046: https://github.com/PaulStoffregen/XPT2046_Touchscreen.git
- *
- * Setup steps:
- * 1. Fill your own SSID_NAME, SSID_PASSWORD, VNC_IP, VNC_PORT and VNC_PASSWORD
- * 2. Change your LCD parameters in Arduino_GFX setting
  ******************************************************************************/
+#define ESP_DRD_USE_SPIFFS true
 
-/* WiFi settings */
-const char *SSID_NAME = "WLAN";
-const char *SSID_PASSWORD = "WLAN_KEY";
+#include <FS.h>
+#include <SPIFFS.h>
+#include <ArduinoJson.h>
 
-const char *VNC_IP = "192.168.4.128";
-const uint16_t VNC_PORT = 5900;
-const char *VNC_PASSWORD = "12345678";
+char VNC_IP[50] = "192.168.4.128";
+uint16_t VNC_PORT = 5900;
+char VNC_PW[50] = "12345678";
+
+#define JSON_CONFIG_FILE "/vnc.json"
+bool shouldSaveConfig = false;
+bool forceConfig = false;
 
 /*******************************************************************************
  * Please config the touch panel in touch.h
+ * is configured there for XPT2046 on AZ-Touch mod
  ******************************************************************************/
 #include "touch.h"
 
 /*******************************************************************************
  * Please config the optional keyboard in keyboard.h
+ * currently no keyboard
  ******************************************************************************/
 #include "keyboard.h"
 
@@ -45,17 +48,10 @@ const char *VNC_PASSWORD = "12345678";
  * ESP32-S2 various dev board  : CS: 34, DC: 38, RST: 33, BL: 21, SCK: 36, MOSI: 35, MISO: nil
  * ESP32-S3 various dev board  : CS: 40, DC: 41, RST: 42, BL: 48, SCK: 36, MOSI: 35, MISO: nil
  * ESP8266 various dev board   : CS: 15, DC:  4, RST:  2, BL:  5, SCK: 14, MOSI: 13, MISO: 12
- * Raspberry Pi Pico dev board : CS: 17, DC: 27, RST: 26, BL: 28, SCK: 18, MOSI: 19, MISO: 16
- * RTL8720 BW16 old patch core : CS: 18, DC: 17, RST:  2, BL: 23, SCK: 19, MOSI: 21, MISO: 20
- * RTL8720_BW16 Official core  : CS:  9, DC:  8, RST:  6, BL:  3, SCK: 10, MOSI: 12, MISO: 11
- * RTL8722 dev board           : CS: 18, DC: 17, RST: 22, BL: 23, SCK: 13, MOSI: 11, MISO: 12
- * RTL8722_mini dev board      : CS: 12, DC: 14, RST: 15, BL: 13, SCK: 11, MOSI:  9, MISO: 10
- * Seeeduino XIAO dev board    : CS:  3, DC:  2, RST:  1, BL:  0, SCK:  8, MOSI: 10, MISO:  9
- * Teensy 4.1 dev board        : CS: 39, DC: 41, RST: 40, BL: 22, SCK: 13, MOSI: 11, MISO: 12
  ******************************************************************************/
 #include <Arduino_GFX_Library.h>
 
-// #define GFX_BL DF_GFX_BL // default backlight pin, you may replace DF_GFX_BL to actual backlight pin
+// Backlight pin on AZ-Touch Mod
 #define GFX_BL 15
 
 #if defined(ESP32)
@@ -63,40 +59,126 @@ const char *VNC_PASSWORD = "12345678";
 Arduino_DataBus *bus = new Arduino_ESP32SPI(4 /* DC */, 5 /* CS */, 18 /* SCK */, 23 /* MOSI */, 19 /* MISO */);
 Arduino_GFX *gfx = new Arduino_ILI9341(bus, 22 /* RST */, 1 /* rotation */);
 #elif defined(ESP8266)
-// don't know the pins, please tell me
+// don't know the pins/ can't test with the ESP8266 , please tell me, should work there too
 #endif
 
-/* More dev device declaration: https://github.com/moononournation/Arduino_GFX/wiki/Dev-Device-Declaration */
-// #if defined(DISPLAY_DEV_KIT)
-// Arduino_GFX *gfx = create_default_Arduino_GFX();
-// #else /* !defined(DISPLAY_DEV_KIT) */
-
-/* More data bus class: https://github.com/moononournation/Arduino_GFX/wiki/Data-Bus-Class */
-// Arduino_DataBus *bus = create_default_Arduino_DataBus();
-
-/* More display class: https://github.com/moononournation/Arduino_GFX/wiki/Display-Class */
-// Arduino_GFX *gfx = new Arduino_ILI9341(bus, DF_GFX_RST, 3 /* rotation */, false /* IPS */);
-
-// #endif /* !defined(DISPLAY_DEV_KIT) */
 /*******************************************************************************
  * End of Arduino_GFX setting
  ******************************************************************************/
 
-#if defined(ESP32)
-#include <WiFi.h>
-#elif defined(ESP8266)
-#include <ESP8266WiFi.h>
-#elif defined(ARDUINO_RASPBERRY_PI_PICO_W)
-#include <WiFi.h>
-#elif defined(RTL8722DM)
-#include <WiFi.h>
-#endif
+// WiFi Manager does all the Wifi config stuff and loads config for VNC-Client
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+WiFiManager wm; // global WifiManager
+
+#define TRIGGER_PIN 0 // use boot pin to force config
 
 #include "VNC_GFX.h"
 #include <VNC.h>
 
 VNC_GFX *vnc_gfx = new VNC_GFX(gfx);
 arduinoVNC vnc = arduinoVNC(vnc_gfx);
+
+
+// save and load config, got it from: https://github.com/witnessmenow/ESP32-WiFi-Manager-Examples
+void saveConfigFile()
+{
+  Serial.println(F("Saving config"));
+  StaticJsonDocument<512> json;
+  json["VNC_IP"] = VNC_IP;
+  json["VNC_PORT"] = VNC_PORT;
+  json["VNC_PW"] = VNC_PW;
+
+  File configFile = SPIFFS.open(JSON_CONFIG_FILE, "w");
+  if (!configFile)
+  {
+    Serial.println("failed to open config file for writing");
+  }
+
+  serializeJsonPretty(json, Serial);
+  if (serializeJson(json, configFile) == 0)
+  {
+    Serial.println(F("Failed to write to file"));
+  }
+  configFile.close();
+}
+
+bool loadConfigFile()
+{
+  // clean FS, for testing
+  // SPIFFS.format();
+
+  //read configuration from FS json
+  Serial.println("mounting FS...");
+
+  // May need to make it begin(true) first time you are using SPIFFS
+  // NOTE: This might not be a good way to do this! begin(true) reformats the spiffs
+  // it will only get called if it fails to mount, which probably means it needs to be
+  // formatted, but maybe dont use this if you have something important saved on spiffs
+  // that can't be replaced.
+  if (SPIFFS.begin(false) || SPIFFS.begin(true))
+  {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists(JSON_CONFIG_FILE))
+    {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open(JSON_CONFIG_FILE, "r");
+      if (configFile)
+      {
+        Serial.println("opened config file");
+        StaticJsonDocument<512> json;
+        DeserializationError error = deserializeJson(json, configFile);
+        serializeJsonPretty(json, Serial);
+        if (!error)
+        {
+          Serial.println("\nparsed json");
+
+          strcpy(VNC_IP, json["VNC_IP"]);
+          VNC_PORT = json["VNC_PORT"].as<int>();
+          strcpy(VNC_PW, json["VNC_PW"]);
+
+          return true;
+        }
+        else
+        {
+          Serial.println("failed to load json config");
+        }
+      }
+    }
+  }
+  else
+  {
+    Serial.println("failed to mount FS");
+  }
+  //end read
+  return false;
+}
+
+void saveConfigCallback()
+{
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+// This gets called when the config mode is launched, might
+// be useful to update a display with this info.
+void configModeCallback(WiFiManager *myWiFiManager)
+{
+  Serial.println("Entered Conf Mode");
+
+  Serial.print("Config SSID: ");
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+
+  // Serial.print("Config Parameter: ");
+  // Serial.println(myWiFiManager->getParameters);
+
+  Serial.print("Config IP Address: ");
+  Serial.println(WiFi.softAPIP());
+
+  
+}
+
+
 
 void TFTnoWifi(void)
 {
@@ -108,6 +190,18 @@ void TFTnoWifi(void)
   gfx->setTextSize(2);
   gfx->println();
 }
+
+void TFT_WM(void)
+{
+  gfx->fillScreen(BLACK);
+  gfx->setCursor(0, ((gfx->height() / 2) - (5 * 8)));
+  gfx->setTextColor(RED);
+  gfx->setTextSize(4);
+  gfx->println("WiFiManager");
+  gfx->setTextSize(2);
+  gfx->println();
+}
+
 
 void TFTnoVNC(void)
 {
@@ -176,58 +270,88 @@ void handle_keyboard()
   }
 }
 
-void setup(void)
-{
+void checkButton(){
+  // check for button press
+  if ( digitalRead(TRIGGER_PIN) == LOW ) {
+    forceConfig=true;
+    Serial.println("Button Pressed");
+    gfx->println("Button pressed...");
+  }
+}
+
+
+void setup() { 
   Serial.begin(115200);
-  // Serial.setDebugOutput(true);
-  // while(!Serial);
-  Serial.println("Arduino VNC");
-
-#ifdef GFX_EXTRA_PRE_INIT
-  GFX_EXTRA_PRE_INIT();
-#endif
-
-  // Init touch device
-  // touch_init(gfx->width(), gfx->height());
-  // Serial.printf("gfx->width: %d, gfx->height: %d\n",gfx->width(), gfx->height());
-
+  Serial.setDebugOutput(true);
+  while(!Serial);
+  Serial.println("AZ-Touch VNC");
   // Init keyboard device
   keyboard_init();
 
   Serial.println("Init display");
   gfx->begin();
   touch_init(gfx->width(), gfx->height());
-  // Serial.printf("gfx->width: %d, gfx->height: %d After gfx->begin\n",gfx->width(), gfx->height());
   gfx->fillScreen(BLACK);
-
 #ifdef GFX_BL
   pinMode(GFX_BL, OUTPUT);
   // AZ-Touch Mod has inverted backlight, so LOW for  backlight on
   digitalWrite(GFX_BL, LOW);
 #endif
   TFTnoWifi();
+ 
 
-  Serial.println("Init WiFi");
-  gfx->println("Init WiFi");
-#if defined(ESP32)
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(SSID_NAME, SSID_PASSWORD);
-#elif defined(ESP8266)
-  // disable sleep mode for better data rate
-  WiFi.setSleepMode(WIFI_NONE_SLEEP);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(SSID_NAME, SSID_PASSWORD);
-#elif defined(ARDUINO_RASPBERRY_PI_PICO_W)
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(SSID_NAME, SSID_PASSWORD);
-#elif defined(RTL8722DM)
-  WiFi.begin((char *)SSID_NAME, (char *)SSID_PASSWORD);
-#endif
-  while (WiFi.status() != WL_CONNECTED)
+  bool spiffsSetup = loadConfigFile();
+  if (!spiffsSetup)
   {
-    delay(500);
-    Serial.print(".");
-    gfx->print(".");
+    TFT_WM();
+    Serial.println(F("There is no saved config"));
+    gfx->println("There is no saved config");
+    SPIFFS.format();
+    forceConfig = true;
+  } else {
+    Serial.println("Press Boot Button for WiFiManager");
+    gfx->println("Press Boot for WiFiManager");
+    delay(1000);
+    checkButton();
+  }
+
+  // Config Fields for VNC
+  WiFiManagerParameter vnc_ip("VNC_IP","IP-Adress of VNC-Server",VNC_IP,50);
+  wm.addParameter(&vnc_ip);
+  char convertedValue[6];
+  sprintf(convertedValue, "%d", VNC_PORT);
+  WiFiManagerParameter vnc_port("VNC_Port","port of VNC-Server",convertedValue,6);
+  wm.addParameter(&vnc_port);
+  WiFiManagerParameter vnc_pw("VNC_PW","password VNC-Server",VNC_PW,50);
+  wm.addParameter(&vnc_pw);
+
+  wm.setSaveConfigCallback(saveConfigCallback);
+  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
+  wm.setAPCallback(configModeCallback);
+
+  bool wm_con;
+  if (forceConfig) {
+    TFT_WM();
+    gfx->println("WLAN: AZ-Touch-VNC");
+    if (!wm.startConfigPortal("AZ-Touch-VNC"))
+    {
+      Serial.println("failed to connect and hit timeout");
+      delay(3000);
+      //reset and try again, or maybe put it to deep sleep
+      ESP.restart();
+      delay(5000);
+    }
+  } else {
+    TFTnoWifi();
+    gfx->println("Waiting for WLAN ");
+    if (!wm.autoConnect("AZ-Touch-VNC"))
+    {
+      Serial.println("failed to connect and hit timeout");
+      delay(3000);
+      // if we still have not connected restart and try all over again
+      ESP.restart();
+      delay(5000);
+    }
   }
   Serial.println(" CONNECTED");
   gfx->println(" CONNECTED");
@@ -237,14 +361,16 @@ void setup(void)
   gfx->println(WiFi.localIP());
   TFTnoVNC();
 
+  //save the custom parameters to FS
+  if (shouldSaveConfig)
+  {
+    saveConfigFile();
+  }
+
   Serial.println(F("[SETUP] VNC..."));
-
-#ifdef SEPARATE_DRAW_TASK
-  draw_task_setup();
-#endif
-
+  VNC_PORT = atoi(vnc_port.getValue());
   vnc.begin(VNC_IP, VNC_PORT);
-  vnc.setPassword(VNC_PASSWORD); // optional
+  vnc.setPassword(VNC_PW); // optional
 }
 
 void loop()
@@ -253,7 +379,10 @@ void loop()
   {
     vnc.reconnect();
     TFTnoWifi();
+    Serial.println("Press Boot Button for WiFiManager");
+    gfx->println("Press Boot Button for WiFiManager");
     delay(100);
+    checkButton();
   }
   else
   {
